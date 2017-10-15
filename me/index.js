aad6a0b8944aa6beb9eecf94ea00be0c823d55fa
Aik Siang <aiksiang.ang@grabtaxi.com>
2017-10-15 17:00:44 +0800

    Fix route

--- me/index.js
+++ me/index.js
@@ -65,7 +65,7 @@
 /******/ 	}
 /******/ 	
 /******/ 	var hotApplyOnUpdate = true;
-/******/ 	var hotCurrentHash = "6d76c1e13f79ca0dbcb5"; // eslint-disable-line no-unused-vars
+/******/ 	var hotCurrentHash = "da9378d98accf476529a"; // eslint-disable-line no-unused-vars
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentParents = []; // eslint-disable-line no-unused-vars
 /******/ 	
@@ -654,7 +654,7 @@
 	  ),
 	  _react2.default.createElement(
 	    _reactRouter.Route,
-	    { path: '/cadenceco/', component: Index },
+	    { path: '/me/', component: Index },
 	    _react2.default.createElement(_reactRouter.IndexRoute, { component: Index })
 	  )
 	), document.getElementById('root'));
